478b9bf0cf8a04fb40e191e500554de913929642
Andrea Maugars <andrea.maugars@gmail.com>
2019-06-19 17:20:26 +0300

    Add update and delete rooms

--- src/database.js
+++ src/database.js
@@ -20,7 +20,12 @@ const database = {
 };
 
 exports.get = resource => database[resource];
-exports.getOne = (resource, id) => database[resource].filter(r => r.id === id)[0];
+exports.getOne = (resource, id) => database[resource].find(r => r.id === id);
+exports.update = (resource, id, data) => {
+    const index = database[resource].findIndex(r => r.id === id);
+    const newItem = { ...database[resource][index], ...data };
+    database[resource][index] = newItem;
+};
 exports.create = (resource, data) => {
     const id = database[resource].length + 1;
     const newResource = {
@@ -29,4 +34,7 @@ exports.create = (resource, data) => {
     };
     database[resource].push(newResource);
     return newResource;
+};
+exports.remove = (resource, id) => {
+    database[resource] = database[resource].filter(r => r.id !== id);
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const multer = require('multer');
-const { get, getOne, create } = require('./database');
+const bodyParser = require('body-parser');
+const {
+    get, getOne, update, create, remove,
+} = require('./database');
 
 const app = express();
 const upload = multer();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!');
@@ -19,10 +25,20 @@ app.get('/rooms/:id', (req, res) => {
     res.status(200).send(response);
 });
 
+app.put('/rooms/:id', (req, res) => {
+    update('rooms', Number(req.params.id), req.body);
+    res.status(200).send({});
+});
+
 app.post('/rooms', upload.none(), (req, res) => {
     const { body } = req;
     const newRoom = create('rooms', body);
     res.status(200).send(newRoom);
+});
+
+app.delete('/rooms/:id', (req, res) => {
+    remove('rooms', Number(req.params.id));
+    res.status(200).send({});
 });
 
 app.listen(3000, () => {
